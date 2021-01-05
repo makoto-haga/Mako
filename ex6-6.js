bfbc17766999d4cfe32f6a82043d2a83c5daaf5e
makoto-haga <ZM18023@josai.ac.jp>
2021-01-05 07:06:27 +0000

    1005

--- ex6-6.js
+++ ex6-6.js
@@ -1,6 +1,5 @@
-var who65 = 'world';
+var who66 = 'world';
 function sayhello66() {alert('Hello, ' + who66 + '!');}
-function taro65() {who66 = '太郎';}
-function hanako65() {who66 = '花子';}
+function taro66() {who66 = '太郎';}
+function hanako66() {who66 = '花子';}
 function gakita(){alert(who66 +'が来た’);}
-
