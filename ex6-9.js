f1e6fec9a4107023190e4b0843d188ff8a5f42a1
makoto-haga <ZM18023@josai.ac.jp>
2021-02-13 14:46:30 +0000

    0213-9

--- ex6-9.js
+++ ex6-9.js
@@ -6,5 +6,7 @@ function imgchange1() {
 
 function show_innerHTMLtxt1(){var result1 = document.getElementById('txt1').innerHTML;alert(result1);}
 
-function show_innerHTMLlogo(){var result= document.getElementById('logo').innerHTML;alert(result);}
+function show_innerHTML(){
+    var result= document.getElementById('logo').innerHTML;
+    alert(result);}
 
